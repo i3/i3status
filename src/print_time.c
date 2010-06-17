da595ee9f7cc487b02109e992e4b2f3f27f78d5a
Axel Wagner <mail@merovius.de>
2010-06-17 21:33:28 +0200

    Do proper error handling in print_time()

--- src/print_time.c
+++ src/print_time.c
@@ -7,7 +7,13 @@ void print_time(const char *format) {
         static char part[512];
         /* Get date & time */
         time_t current_time = time(NULL);
+        if (current_time == (time_t) -1) {
+                return;
+        }
         struct tm *current_tm = localtime(&current_time);
+        if (current_tm == NULL) {
+                return;
+        }
         (void)strftime(part, sizeof(part), format, current_tm);
         printf("%s", part);
 }
