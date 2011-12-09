7148eb52790f78e4e4fb60555d19c5c958f271c7
Baptiste Daroussin <bapt@FreeBSD.org>
2011-12-10 00:30:22 +0100

    FreeBSD: switch from acpi to coretemp to get the cpu temperature

--- include/i3status.h
+++ include/i3status.h
@@ -16,7 +16,8 @@ enum { O_DZEN2, O_XMOBAR, O_NONE } output_format;
 
 #elif defined(__FreeBSD__) || defined(__FreeBSD_kernel__)
 
-#define THERMAL_ZONE "hw.acpi.thermal.tz%d.temperature"
+/* this needs the coretemp module to be loaded */
+#define THERMAL_ZONE "dev.cpu.%d.temperature"
 #define BATT_LIFE "hw.acpi.battery.life"
 #define BATT_TIME "hw.acpi.battery.time"
 #define BATT_STATE "hw.acpi.battery.state"
