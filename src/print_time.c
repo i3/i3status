78cd0e1e0165b59638e4bf0af2e48612bd35fb1b
Michael Stapelberg <michael@stapelberg.de>
2013-02-10 17:19:56 +0100

    s/\<timezone\>/tz/ in order to not shadow the timezone var from time.h

--- src/print_time.c
+++ src/print_time.c
@@ -11,33 +11,33 @@ static int local_timezone_init = 0;
 static const char *local_timezone = NULL;
 static const char *current_timezone = NULL;
 
-void set_timezone(const char *timezone) {
+void set_timezone(const char *tz) {
         if (!local_timezone_init) {
                 /* First call, initialize. */
                 local_timezone = getenv("TZ");
                 local_timezone_init = 1;
         }
-        if (timezone == NULL || timezone[0] == '\0') {
+        if (tz == NULL || tz[0] == '\0') {
                 /* User wants localtime. */
-                timezone = local_timezone;
+                tz = local_timezone;
         }
-        if (timezone != current_timezone) {
-                if (timezone) {
-                        setenv("TZ", timezone, 1);
+        if (tz != current_timezone) {
+                if (tz) {
+                        setenv("TZ", tz, 1);
                 } else {
                         unsetenv("TZ");
                 }
                 tzset();
-                current_timezone = timezone;
+                current_timezone = tz;
         }
 }
 
-void print_time(yajl_gen json_gen, char *buffer, const char *format, const char *timezone, time_t t) {
+void print_time(yajl_gen json_gen, char *buffer, const char *format, const char *tz, time_t t) {
         char *outwalk = buffer;
         struct tm tm;
 
         /* Convert time and format output. */
-        set_timezone(timezone);
+        set_timezone(tz);
         localtime_r(&t, &tm);
         outwalk += strftime(outwalk, 4095, format, &tm);
         *outwalk = '\0';
