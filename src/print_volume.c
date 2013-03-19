f30ec89b79143d5d90f7fbe9c5aaf73ef6847e30
Baptiste Daroussin <bapt@FreeBSD.org>
2013-03-19 18:32:08 +0100

    Support mixer_idx with OSS
    make mixer_idx load the default mixer

--- src/print_volume.c
+++ src/print_volume.c
@@ -175,12 +175,22 @@ void print_volume(yajl_gen json_gen, char *buffer, const char *fmt, const char *
 	}
 #endif
 #if defined(__FreeBSD__) || defined(__OpenBSD__) || defined(__DragonFly__)
-        char mixerpath[] = "/dev/mixer";
+        char *mixerpath;
+        char defaultmixer[] = "/dev/mixer";
         int mixfd, vol, devmask = 0;
         pbval = 1;
 
+        if (mixer_idx > 0)
+                asprintf(&mixerpath, "/dev/mixer%d", mixer_idx);
+        else
+                mixerpath = defaultmixer;
+
         if ((mixfd = open(mixerpath, O_RDWR)) < 0)
                 return;
+
+        if (mixer_idx > 0)
+                free(mixerpath);
+
         if (ioctl(mixfd, SOUND_MIXER_READ_DEVMASK, &devmask) == -1)
                 return;
         if (ioctl(mixfd, MIXER_READ(0),&vol) == -1)
