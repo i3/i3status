7dad3150db99f4ff5a7193a76a62995fd9a894e5
jefvel <geekku@gmail.com>
2015-03-21 20:30:12 +0100

    checks if format_load is set. if not, it will use the default format

--- i3status.c
+++ i3status.c
@@ -362,7 +362,7 @@ int main(int argc, char *argv[]) {
 
     cfg_opt_t load_opts[] = {
         CFG_STR("format", "%1min %5min %15min", CFGF_NONE),
-        CFG_STR("format_load", "%1min %5min %15min", CFGF_NONE),
+        CFG_STR("format_load", NULL, CFGF_NONE),
         CFG_FLOAT("max_threshold", 5, CFGF_NONE),
         CFG_CUSTOM_ALIGN_OPT,
         CFG_CUSTOM_COLOR_OPTS,
@@ -622,7 +622,13 @@ int main(int argc, char *argv[]) {
 
             CASE_SEC("load") {
                 SEC_OPEN_MAP("load");
-                print_load(json_gen, buffer, cfg_getstr(sec, "format"), cfg_getstr(sec, "format_load"), cfg_getfloat(sec, "max_threshold"));
+                const char* format_load = cfg_getstr(sec, "format_load");
+                const char* format = cfg_getstr(sec, "format");
+                if(format_load == NULL) {
+                    print_load(json_gen, buffer, format, format, cfg_getfloat(sec, "max_threshold"));
+                } else {
+                    print_load(json_gen, buffer, format, format_load, cfg_getfloat(sec, "max_threshold"));
+                }
                 SEC_CLOSE_MAP;
             }
 
