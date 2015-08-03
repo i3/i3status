08049ad6546b6aa9dc87f4fc234ededb5a9d4ea6
Dmitrij D. Czarkoff <czarkoff@gmail.com>
2015-08-03 23:03:39 +0200

    Make checker smarter
     * In "Status" header only check for "O".
     * Remember previous mtime and message number of mailbox; if unchange, avoid
       reading mbox itself.

--- src/print_mbox_info.c
+++ src/print_mbox_info.c
@@ -13,6 +13,12 @@
 
 #define LINELEN 1000
 
+/* These variables are used to store data between calls to print_mbox_info().
+ * The purpose of such */
+struct timeval times[2];
+size_t messages = 0;
+bool initialized = false;
+
 /*
  * Check whether path references regular file, and if it does, parse the file,
  * search for continuous blocks of text starting with unquoted "From " line.
@@ -27,84 +33,108 @@ void print_mbox_info(yajl_gen json_gen, char *buffer, const char *path, const ch
     char *outwalk = buffer;
     const char *walk = format;
     struct stat sb;
-    struct timeval times[2];
     bool exists = !stat(path, &sb);
     bool after_newline = true, in_header = false;
     FILE *f;
     char s[LINELEN];
-    size_t messages = 0;
 
     INSTANCE(path);
 
-    if (exists && S_ISREG(sb.st_mode) && sb.st_size != 0 && sb.st_atime <= sb.st_mtime) {
-        /* Read the file and count the "From" header instances */
-        if ((f = fopen(path, "rw")) == NULL) {
-            START_COLOR("color_bad");
-            outwalk += sprintf(outwalk, "mbox: ");
-            (void)strerror_r(errno, outwalk, sizeof(buffer) - (outwalk - buffer));
-            outwalk = buffer + strnlen(buffer, sizeof(buffer));
-            goto out;
-        }
-        while (fgets(s, LINELEN, f) != NULL) {
-            if (after_newline && BEGINS_WITH(s, "From ")) {
-                messages++;
-                in_header = true;
-            } else if (in_header && BEGINS_WITH(s, "Status: RO")) {
-                messages--;
+    if (!initialized) {
+        times[1].tv_sec = 0;
+        initialized = true;
+    }
+
+    if ((exists && S_ISREG(sb.st_mode)) || !exists) {
+        if (!exists || sb.st_size == 0 || (sb.st_mtim.tv_sec == times[1].tv_sec && messages == 0)) {
+            if (format_no_mail) {
+                START_COLOR("color_good");
+                outwalk += sprintf(outwalk, "%s", format_no_mail);
+                END_COLOR;
             }
-            
-            if (*s == '\n') {
-                after_newline = true;
-                in_header = false;
+        } else if (exists) {
+            /* If modification time didn't change, apparently the contents is
+             * the same; avoid disk operations then */
+            if (sb.st_mtim.tv_sec != times[1].tv_sec) {
+                messages = 0;
+                /* Read the file and count the "From" header instances */
+                if ((f = fopen(path, "rw")) == NULL) {
+                    START_COLOR("color_bad");
+                    outwalk += sprintf(outwalk, "mbox: ");
+                    (void)strerror_r(errno, outwalk, sizeof(buffer) - (outwalk - buffer));
+                    outwalk = buffer + strnlen(buffer, sizeof(buffer));
+                    END_COLOR;
+                    goto out;
+                }
+                while (fgets(s, LINELEN, f) != NULL) {
+                    if (after_newline && BEGINS_WITH(s, "From ")) {
+                        messages++;
+                        in_header = true;
+                    } else if (in_header && BEGINS_WITH(s, "Status:") && strchr(s, 'O') != NULL) {
+                        messages--;
+                    }
+
+                    if (*s == '\n') {
+                        after_newline = true;
+                        in_header = false;
+                    } else {
+                        after_newline = false;
+                    }
+                }
+
+                /* If fgets(3) returned NULL due to some error, message count
+                 * is likely wrong.  Print error instead of message count */
+                if (!feof(f)) {
+                    (void)fclose(f);
+                    START_COLOR("color_bad");
+                    outwalk += sprintf(outwalk, "mbox: ");
+                    (void)strerror_r(errno, outwalk, sizeof(buffer) - (outwalk - buffer));
+                    outwalk = buffer + strnlen(buffer, sizeof(buffer));
+                    END_COLOR;
+                    goto out;
+                }
+                (void)fclose(f);
+
+                /* Restore access and modification times from stat(2) call
+                 * and apply back to the file */
+                TIMESPEC_TO_TIMEVAL(&times[0], &(sb.st_atim))
+                TIMESPEC_TO_TIMEVAL(&times[1], &sb.st_mtim)
+                (void)utimes(path, times);
             } else {
-                after_newline = false;
             }
-        }
+            if (messages == 0) {
+                if (format_no_mail) {
+                    START_COLOR("color_good");
+                    outwalk += sprintf(outwalk, "%s", format_no_mail);
+                    END_COLOR;
+                }
+            } else {
+                START_COLOR("color_degraded");
+                for (; *walk != '\0'; walk++) {
+                    if (*walk != '%') {
+                        *(outwalk++) = *walk;
+                        continue;
+                    }
 
-        /* If fgets(3) returned NULL due to some error, message count
-         * is likely wrong.  Print error instead of message count */
-        if (!feof(f)) {
-            (void)fclose(f);
-            START_COLOR("color_bad");
-            outwalk += sprintf(outwalk, "mbox: ");
-            (void)strerror_r(errno, outwalk, sizeof(buffer) - (outwalk - buffer));
-            outwalk = buffer + strnlen(buffer, sizeof(buffer));
-            goto out;
-        }
-        (void)fclose(f);
+                    if (BEGINS_WITH(walk + 1, "mails")) {
+                        outwalk += sprintf(outwalk, "%zu", messages);
+                        walk += strlen("mails");
+                    }
 
-        /* Restore access and modification times from stat(2) call
-         * and apply back to the file */
-        TIMESPEC_TO_TIMEVAL(&times[0], &(sb.st_atim))
-        TIMESPEC_TO_TIMEVAL(&times[1], &sb.st_mtim)
-        (void)utimes(path, times);
-
-        START_COLOR("color_degraded");
-        for (; *walk != '\0'; walk++) {
-            if (*walk != '%') {
-                *(outwalk++) = *walk;
-                continue;
-            }
-
-            if (BEGINS_WITH(walk + 1, "mails")) {
-                outwalk += sprintf(outwalk, "%zu", messages);
-                walk += strlen("mails");
-            }
-
-            if (BEGINS_WITH(walk + 1, "%")) {
-                outwalk += sprintf(outwalk, "%%");
-                walk += strlen("%");
+                    if (BEGINS_WITH(walk + 1, "%")) {
+                        outwalk += sprintf(outwalk, "%%");
+                        walk += strlen("%");
+                    }
+                }
+                END_COLOR;
             }
         }
     } else if (exists && !S_ISREG(sb.st_mode)) {
         START_COLOR("color_bad");
         outwalk += sprintf(outwalk, "%s is not an mbox", path);
-    } else if (format_no_mail) {
-        START_COLOR("color_good");
-        outwalk += sprintf(outwalk, "%s", format_no_mail);
+        END_COLOR;
     }
 
 out:
-    END_COLOR;
     OUTPUT_FULL_TEXT(buffer);
 }
