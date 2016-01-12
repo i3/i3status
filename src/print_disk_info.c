i3/i3status

commit 75a835742e60311f7f27bf44af862caad67e3a23

Use format_not_mounted if the directory for a disk_info block does not exist. This is useful for dynamically mounted devices like thumb drives that get mounted into a directory such as '/media/$UID/$NAME'.
fixes #33

src/print_disk_info.c
@@ -124,10 +124,11 @@ void print_disk_info(yajl_gen json_gen, char *buffer, const char *path, const ch
 #else
     struct statvfs buf;
 
-    if (statvfs(path, &buf) == -1)
-        return;
-
-    if (format_not_mounted != NULL) {
+    if (statvfs(path, &buf) == -1) {
+        /* If statvfs errors, e.g., due to the path not existing,
+         * we use the format for a not mounted device. */
+        format = format_not_mounted;
+    } else if (format_not_mounted != NULL) {
         FILE *mntentfile = setmntent("/etc/mtab", "r");
         struct mntent *m;
         bool found = false;

man/i3status.man
@@ -238,7 +238,7 @@ is assumed to be "percentage_avail" and low_threshold to be set to 0, which
 implies no coloring at all.
 
 You can define a different format with the option "format_not_mounted"
-which is used if the path is not a mount point. So you can just empty
+which is used if the path does not exist or is not a mount point. So you can just empty
 the output for the given path with adding +format_not_mounted=""+
 to the config section.
 

commit fad9c8237c63290aa9adde5ba6ffa6759f578645

Bugfix: check that format_not_mounted is not NULL before using it
This issue was introduced with commit 75a83574.

fixes #92

src/print_disk_info.c
@@ -127,7 +127,9 @@ void print_disk_info(yajl_gen json_gen, char *buffer, const char *path, const ch
     if (statvfs(path, &buf) == -1) {
         /* If statvfs errors, e.g., due to the path not existing,
          * we use the format for a not mounted device. */
-        format = format_not_mounted;
+        if (format_not_mounted != NULL) {
+            format = format_not_mounted;
+        }
     } else if (format_not_mounted != NULL) {
         FILE *mntentfile = setmntent("/etc/mtab", "r");
         struct mntent *m;