93576bccc3acff95206933cc1486e0d01c15316e
Baptiste Daroussin <baptiste.daroussin@gmail.com>
2017-02-01 20:28:01 +0100

    Fix the deciKelvin to Celsius conversion on FreeBSD (#195)
    Change from ceiled value to floored value to be consistent with the
    FreeBSD kernel
    
    https://svnweb.freebsd.org/base?view=revision&revision=300421

--- src/print_cpu_temperature.c
+++ src/print_cpu_temperature.c
@@ -13,7 +13,7 @@
 #include <err.h>
 #include <sys/types.h>
 #include <sys/sysctl.h>
-#define TZ_ZEROC 2732
+#define TZ_ZEROC 2731
 #define TZ_KELVTOC(x) (((x)-TZ_ZEROC) / 10), abs(((x)-TZ_ZEROC) % 10)
 #define TZ_AVG(x) ((x)-TZ_ZEROC) / 10
 #endif
