42e714729178679b425095bc1acb1b143ed4885c
Felix Buehler <account@buehler.rocks>
2020-02-10 22:33:20 +0100

    add wireless format-options

--- i3status.c
+++ i3status.c
@@ -338,7 +338,10 @@ int main(int argc, char *argv[]) {
     cfg_opt_t wireless_opts[] = {
         CFG_STR("format_up", "W: (%quality at %essid, %bitrate) %ip", CFGF_NONE),
         CFG_STR("format_down", "W: down", CFGF_NONE),
+        CFG_STR("format_bitrate", "%g %cb/s", CFGF_NONE),
+        CFG_STR("format_noise", "%3d%s", CFGF_NONE),
         CFG_STR("format_quality", "%3d%s", CFGF_NONE),
+        CFG_STR("format_signal", "%3d%s", CFGF_NONE),
         CFG_CUSTOM_ALIGN_OPT,
         CFG_CUSTOM_COLOR_OPTS,
         CFG_CUSTOM_MIN_WIDTH_OPT,
@@ -732,7 +735,7 @@ int main(int argc, char *argv[]) {
                     interface = first_eth_interface(NET_TYPE_WIRELESS);
                 if (interface == NULL)
                     interface = title;
-                print_wireless_info(json_gen, buffer, interface, cfg_getstr(sec, "format_up"), cfg_getstr(sec, "format_down"), cfg_getstr(sec, "format_quality"));
+                print_wireless_info(json_gen, buffer, interface, cfg_getstr(sec, "format_up"), cfg_getstr(sec, "format_down"), cfg_getstr(sec, "format_bitrate"), cfg_getstr(sec, "format_noise"), cfg_getstr(sec, "format_quality"), cfg_getstr(sec, "format_signal"));
                 SEC_CLOSE_MAP;
             }
 
