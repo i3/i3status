i3/i3status

commit f0b5758c72f331f598d18f6127e3d55d803af5d7

use format_placeholder for file_content

src/print_file_contents.c
@@ -12,6 +12,8 @@
 #include <errno.h>
 #include "i3status.h"
 
+#define STRING_SIZE 10
+
 static void *scalloc(size_t size) {
     void *result = calloc(size, 1);
     if (result == NULL) {
@@ -42,32 +44,32 @@ void print_file_contents(yajl_gen json_gen, char *buffer, const char *title, con
         START_COLOR("color_bad");
     }
 
-    for (; *walk != '\0'; walk++) {
-        if (*walk != '%') {
-            *(outwalk++) = *walk;
-        } else if (BEGINS_WITH(walk + 1, "title")) {
-            outwalk += sprintf(outwalk, "%s", title);
-            walk += strlen("title");
-        } else if (BEGINS_WITH(walk + 1, "content")) {
-            for (char *s = buf; *s != '\0' && n > 0; s++, n--) {
-                if (*s != '\n') {
-                    *(outwalk++) = *s;
-                }
-            }
-            walk += strlen("content");
-        } else if (BEGINS_WITH(walk + 1, "errno")) {
-            outwalk += sprintf(outwalk, "%d", errno);
-            walk += strlen("errno");
-        } else if (BEGINS_WITH(walk + 1, "error")) {
-            outwalk += sprintf(outwalk, "%s", strerror(errno));
-            walk += strlen("error");
-        } else {
-            *(outwalk++) = '%';
+    // remove newline chars
+    char *src, *dst;
+    for (src = dst = buf; *src != '\0'; src++) {
+        *dst = *src;
+        if (*dst != '\n') {
+            dst++;
         }
     }
+    *dst = '\0';
+
+    char string_errno[STRING_SIZE];
+
+    sprintf(string_errno, "%d", errno);
+
+    placeholder_t placeholders[] = {
+        {.name = "%title", .value = title},
+        {.name = "%content", .value = buf},
+        {.name = "%errno", .value = string_errno},
+        {.name = "%error", .value = strerror(errno)}};
+
+    const size_t num = sizeof(placeholders) / sizeof(placeholder_t);
+    buffer = format_placeholders(walk, &placeholders[0], num);
 
     free(buf);
 
     END_COLOR;
     OUTPUT_FULL_TEXT(buffer);
+    free(buffer);
 }

commit 8598a76681ae0fc58b33f1e6ac4eecf0f71c326f

print_file_contents: fix colors
Commit
https://github.com/i3/i3status/commit/f0b5758c72f331f598d18f6127e3d55d803af5d7
broke the END_COLOR macro because the outwalk variable was not updated.

src/print_file_contents.c
@@ -59,11 +59,12 @@ void print_file_contents(yajl_gen json_gen, char *buffer, const char *title, con
         {.name = "%error", .value = strerror(errno)}};
 
     const size_t num = sizeof(placeholders) / sizeof(placeholder_t);
-    buffer = format_placeholders(walk, &placeholders[0], num);
-
+    char *formatted = format_placeholders(walk, &placeholders[0], num);
+    strcpy(outwalk, formatted);
+    outwalk += strlen(formatted);
+    free(formatted);
     free(buf);
 
     END_COLOR;
     OUTPUT_FULL_TEXT(buffer);
-    free(buffer);
 }