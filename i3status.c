a8588c4af173b01920543da0ae72a74c0d7a8ef0
Alex Marchi <alex.marchi2000@gmail.com>
2025-08-19 09:05:24 +0200

    Fix format_quality string to use a single digit placeholder

--- i3status.c
+++ i3status.c
@@ -285,7 +285,7 @@ int main(int argc, char *argv[]) {
         CFG_STR("format_down", "W: down", CFGF_NONE),
         CFG_STR("format_bitrate", "%g %cb/s", CFGF_NONE),
         CFG_STR("format_noise", "%3d%s", CFGF_NONE),
-        CFG_STR("format_quality", "%3d%s", CFGF_NONE),
+        CFG_STR("format_quality", "%d%s", CFGF_NONE),
         CFG_STR("format_signal", "%3d%s", CFGF_NONE),
         CFG_CUSTOM_ALIGN_OPT,
         CFG_CUSTOM_COLOR_OPTS,
